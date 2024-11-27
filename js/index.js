4ae26179cf18e6bc627ff7b7792827f2a1f8bf7a
Alvaro Luque Morillo <aluqmor651@ieszaidinvergeles.org>
2024-11-27 17:35:19 +0100

    Usar cartaFactory y eliminar cartas de index.html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 import { uiDrag } from "../js/uiDrag.js";
-// Permitir el arrastre dentro del contenedor
-uiDrag.init(".contenedor",".carta");
+import { CartaFactory } from "../js/cartaFactory.js";  
+
+document.addEventListener("DOMContentLoaded", () => {
+    const contenedorCartas = document.querySelector('.contenedor-cartas');
+    const palos = ['bastos', 'espadas', 'copas', 'oros'];
+    const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    palos.forEach(palo => {
+        numeros.forEach(numero => {
+            const carta = CartaFactory.crearCarta(palo, numero);
+            contenedorCartas.appendChild(carta);
+        });
+    });
+
+    // Permitir el arrastre dentro del contenedor
+    uiDrag.init(".contenedor", ".carta");
+});
