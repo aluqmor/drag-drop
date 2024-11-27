4e215428a1c8f4f8b5450c5200ad022f4d87ff64
Alvaro Luque Morillo <aluqmor651@ieszaidinvergeles.org>
2024-11-27 17:25:06 +0100

    Crear factoria de cartas

---
+++ js/cartaFactory.js
@@ -0,0 +1,10 @@
+export class CartaFactory {
+    static crearCarta(palo, numero) {
+        const carta = document.createElement('div');
+        carta.classList.add('carta', palo);
+        carta.dataset.palo = palo;
+        carta.id = `${palo}${numero}`;
+        carta.textContent = numero;
+        return carta;
+    }
+}
